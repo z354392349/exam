df53064063069006820f10515d78c6b77b9b5f15
zhangjw <48988840@qq.com>
2020-07-14 09:45:53 +0800

    是

--- a.php
+++ a.php
@@ -1,7 +1,9 @@
+
 <?php
+
     $con = mysql_connect("locahost:3306","root","root") or  die("连接服务器失败") ;
     
-    mysql_select_db('mysql_test', $con) or die("选择数据库失败");
+    mysql_select_db("mysql_test", $con) or die("选择数据库失败");
     mysql_query("set names 'gbk'");
     $res = mysql_query("select * from cust", $con);
 
@@ -15,25 +17,26 @@
         echo "查询失败"
     }
 
-
+    mysql_close($con) or die("关闭服务器失败");
+    
 >
 <?php
+    $con =mysql_connect("localhost:3306","root","pwd") or die("服务器连接失败");
 
-    $con = mysql_connect("localhost:3306","root","root") or die("数据库连接失败");
-
-    mysql_select_db("mysql",$con) or die("数据库选择失败");
+    mysql_select_db("mysql_test", $con) or die("服务器选择失败");
 
     mysql_query("set names 'gbk'");
 
-    $res=mysql_query("select * from cust", $con); 
+    $res = mysql_query("select * from cust", $con);
 
-if($res){
+    if($res){
 
-    echo "数据查询成功<br>";
-
-    $arr= mysql_fetch_array($res,MYSQL_NUM);
-    
-}
-
-
+        $arr= mysql_fetch_array($res, MYSQL_NUM);
+        if($arr){
+            echo "老师".arr[0]
+        }
+    }else{
+        echo
+    }
+ 
 >
