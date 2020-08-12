1eceed26c9d36b22954260d02eaf46172c4d7065
zhangjw <48988840@qq.com>
2020-08-12 17:30:46 +0800

    x

--- a.php
+++ a.php
@@ -20,23 +20,27 @@
     mysql_close($con) or die("关闭服务器失败");
     
 >
-<?php
-    $con =mysql_connect("localhost:3306","root","pwd") or die("服务器连接失败");
 
-    mysql_select_db("mysql_test", $con) or die("服务器选择失败");
 
-    mysql_query("set names 'gbk'");
+<? php
+ $con= mysql_connect("localhost:3306","root", 'pss') or die("数据库连接失败");
+ mysql_select_db("cust",$con) or  die ("s数据库选择失败");
+ mysql_query("set names 'gbl'");
 
-    $res = mysql_query("select * from cust", $con);
-
-    if($res){
-
-        $arr= mysql_fetch_array($res, MYSQL_NUM);
-        if($arr){
-            echo "老师".arr[0]
-        }
-    }else{
-        echo
+ $res = mysql_query("select * from  cust" $con);
+    
+ if($res){
+    $arr= mysql_fetch_array($res,MYSQL_NUM);
+    if($arr){
+        echo "是".$arr[0];
+    } else{
+        echo "查询失败";
     }
- 
+
+ }
+
+
+
 >
+
+
